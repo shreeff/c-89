1556205d83112d1726409c56fd1916ba1dc9d367
shreeff <90686431+shreeff@users.noreply.github.com>
2022-05-10 13:57:11 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDk1u102By80TQNisyGt-tkL3A5ofRjbTM",
+    authDomain: "spectagram-cd94e.firebaseapp.com",
+    databaseURL: "https://spectagram-cd94e-default-rtdb.firebaseio.com",
+    projectId: "spectagram-cd94e",
+    storageBucket: "spectagram-cd94e.appspot.com",
+    messagingSenderId: "553540058477",
+    appId: "1:553540058477:web:82fb6badab51fbf8b62e64"
+  };
+   
